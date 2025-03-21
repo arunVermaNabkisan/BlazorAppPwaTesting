0a067db0555d1dea796462c1470779bf92016784
Arun Kumar Vema <arun.verma@nabkisan.org>
2025-03-21 15:53:42 +0530

    Add project files.

---
+++ BusinessLogic/StudentDataBAL.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessLogic;
+
+public class StudentDataBAL
+{
+    public async Task<int[]> GetStudentDataAsync()
+    {
+        await Task.CompletedTask;
+        return [2, 7, 9, 3];
+    }
+}
