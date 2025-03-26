46cd423bd6b684bc46f7db8979d773aaac7e05b5
Arun Kumar Vema <arun.verma@nabkisan.org>
2025-03-26 14:18:55 +0530

    location

--- BusinessLogic/StudentDataBAL.cs
+++ BusinessLogic/StudentDataBAL.cs
@@ -1,4 +1,5 @@
-﻿namespace BusinessLogic;
+﻿using System.Net.Http.Headers;
+namespace BusinessLogic;
 
 public class StudentDataBAL
 {
@@ -6,5 +7,27 @@ public class StudentDataBAL
     {
         await Task.CompletedTask;
         return [2, 7, 9, 3];
+    }
+
+    public async Task<dynamic> getRapido()
+    {
+        var body = "";
+        var client = new HttpClient();
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri("https://dummy.restapiexample.com/api/v1/employees"),
+            Headers =
+    {
+        { "accept", "application/json" }
+    },
+        };
+        using (var response = await client.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+            body = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(body);
+        }
+        return body;
     }
 }
